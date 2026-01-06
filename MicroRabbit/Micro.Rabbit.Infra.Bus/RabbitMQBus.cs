a41212a20e1eee9151201b599802c07a1a76173f
Mohamed Yehia <m.yehia@MCSFreeZone.onmicrosoft.com>
2026-01-06 13:57:46 +0200

    Implementation of RabbitMQ Bus Comleted

---
+++ MicroRabbit/Micro.Rabbit.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,115 @@
+﻿using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Micro.Rabbit.Infra.Bus
+{
+    public sealed class RabbitMQBus : IEventBus
+    {
+        private readonly IMediator _mediator;
+        private readonly Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventTypes; 
+        public RabbitMQBus(IMediator mediator)
+        {
+            _mediator = mediator;
+            _handlers = new Dictionary<string, List<Type>>();
+            _eventTypes = new List<Type>();
+        }
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
+        public async void PublishAsync<T>(T @event) where T : Event
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = await factory.CreateConnectionAsync())
+            using(var channel = await connection.CreateChannelAsync())
+            {
+                var eventName = @event.GetType().Name;
+                await channel.QueueDeclareAsync(queue: eventName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                var message = JsonConvert.SerializeObject(@event);
+                var body = Encoding.UTF8.GetBytes(message);
+                await channel.BasicPublishAsync(
+                    exchange: "",
+                    routingKey: eventName,
+                    mandatory: false,
+                    body: body);
+            }
+        }
+
+        public void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            var eventName = typeof(T).Name;
+            var handlerType = typeof(TH);
+            if(!_eventTypes.Contains(typeof(T)))
+            {
+                _eventTypes.Add(typeof(T));
+            }
+            if(!_handlers.ContainsKey(eventName))
+            {
+                _handlers.Add(eventName, new List<Type>());
+            }
+            if(_handlers[eventName].Any(s => s.GetType() == handlerType))
+            {
+                throw new ArgumentException($"Handler Type {handlerType.Name} already is registered for '{eventName}'", nameof(handlerType));
+            }
+            StartBasicConsume<T>();
+        }
+
+        private async void StartBasicConsume<T>() where T : Event
+        {
+            var factory = new ConnectionFactory() { 
+                HostName = "localhost" };
+            var connection = await factory.CreateConnectionAsync();
+            var channel = await connection.CreateChannelAsync();
+            var eventName = typeof(T).Name;
+            await channel.QueueDeclareAsync(queue: eventName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var Consumer = new AsyncEventingBasicConsumer(channel);
+            Consumer.ReceivedAsync += Consumer_Received;
+            await channel.BasicConsumeAsync(eventName, true, Consumer);
+
+        }
+
+        private async Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
+        {
+            var eventName = @event.RoutingKey;
+            var message = Encoding.UTF8.GetString(@event.Body.ToArray());
+            try
+            {
+                await ProcessEvent(eventName,message).ConfigureAwait(false);
+            }catch (Exception ex)
+            {
+
+            }
+        }
+
+        private async Task ProcessEvent(string eventName, string message)
+        {
+            if(_handlers.ContainsKey(eventName))
+            {
+                var subscriptions = _handlers[eventName];
+                foreach(var subscription in subscriptions)
+                {
+                    var handler = Activator.CreateInstance(subscription);
+                    if(handler == null) continue;
+                    var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
+                    var @event = JsonConvert.DeserializeObject(message, eventType);
+                    var concreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
+                    await (Task)concreteType.GetMethod("Handle")!.Invoke(handler, new object[] { @event })!;
+                }
+            }
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MesssageType { get; protected set; }
+        protected Message()
+        {
+            MesssageType = GetType().Name;
+        }
+    }
+}
