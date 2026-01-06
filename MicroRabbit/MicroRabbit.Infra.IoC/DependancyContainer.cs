911cedc9148dad14d9e9b824c2e928306f2b1187
Mohamed Yehia <m.yehia@MCSFreeZone.onmicrosoft.com>
2026-01-06 14:09:57 +0200

    Initial Commit of Infrastructure Dependency Project

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependancyContainer.cs
@@ -0,0 +1,20 @@
+﻿using Micro.Rabbit.Infra.Bus;
+using MicroRabbit.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependancyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+        }
+    }
+}
